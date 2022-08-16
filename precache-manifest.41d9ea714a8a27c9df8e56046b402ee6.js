43f10d01f4938e7f6d5e456a5263dd0ede090155
Victor Bacega <victor.faria@raizen.com>
2022-08-16 20:23:19 -0300

    Updates

---
+++ precache-manifest.41d9ea714a8a27c9df8e56046b402ee6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e1ddc5c78d3bdfc07e76acf46c0f94",
+    "url": "/githubex-pages/index.html"
+  },
+  {
+    "revision": "32d3b0ee5ee5dd055757",
+    "url": "/githubex-pages/static/js/2.708bd5a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/githubex-pages/static/js/2.708bd5a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cc1200691385374b380",
+    "url": "/githubex-pages/static/js/main.ea156bdb.chunk.js"
+  },
+  {
+    "revision": "95a4905c438326b0763a",
+    "url": "/githubex-pages/static/js/runtime-main.e07bc9e4.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/githubex-pages/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/githubex-pages/static/media/logo.04f6798f.svg"
+  }
+]);
